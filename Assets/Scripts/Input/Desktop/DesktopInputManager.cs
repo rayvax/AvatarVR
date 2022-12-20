c3593e15f00d84076ab1a1ee3a6bd871b16e7f7e
rayvax <57082766+rayvax@users.noreply.github.com>
2022-12-20 19:58:04 +0300

    shooting pose + fire hands

--- Assets/Scripts/Input/Desktop/DesktopInputManager.cs
+++ Assets/Scripts/Input/Desktop/DesktopInputManager.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class DesktopInputManager : MonoBehaviour
 {
-  [SerializeField] private SimpleActiveState _stopPoseSelector;
-  [SerializeField] private SimpleActiveState _bunnyPoseSelector;
+  [SerializeField] private SimpleActiveState _rightShootPose;
+  [SerializeField] private SimpleActiveState _leftShootPose;
 
   private void Update()
   {
-    DetectSelectorMouseInput(0, _stopPoseSelector);
-    DetectSelectorMouseInput(1, _bunnyPoseSelector);
+    DetectSelectorMouseInput(0, _rightShootPose);
+    DetectSelectorMouseInput(1, _leftShootPose);
   }
 
   private void DetectSelectorMouseInput(int mouseButton, SimpleActiveState selector)
--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -5,22 +5,25 @@ public class PlayerShooter : MonoBehaviour
 {
   [Header("Poses")]
   [SerializeField] private ActiveStateSelector _fireballAppearPose;
-  [SerializeField] private ActiveStateSelector _shootPose;
+  // [SerializeField] private ActiveStateSelector _shootPose;
 
   [Header("Fireball")]
   [SerializeField] private Fireball _fireballPrefab;
+  [SerializeField] private ActiveComponentDispenser _fireballReadyEffectDispenser;
   [SerializeField] private ActiveComponentDispenser _fireballShootingPositionDispenser;
   [SerializeField] private ActiveComponentDispenser _fireballParentDispenser;
   [SerializeField] private ActiveComponentDispenser _fireballShootingDirectionDispenser;
 
+  private ParticleSystem _fireballReadyEffect;
   private Transform _fireballShootingPosition;
   private Transform _fireballParent;
   private Transform _fireballShootingDirection;
 
-  private Fireball _currentFireball = null;
+  private bool _isFireballReady = false;
 
   private void Start()
   {
+    _fireballReadyEffect = _fireballReadyEffectDispenser.DispenseComponent<ParticleSystem>();
     _fireballShootingPosition
           = _fireballShootingPositionDispenser.DispenseComponent<Transform>();
     _fireballParent = _fireballParentDispenser.DispenseComponent<Transform>();
@@ -30,45 +33,35 @@ public class PlayerShooter : MonoBehaviour
 
   private void OnEnable()
   {
-    _fireballAppearPose.WhenSelected += CreateFireball;
-    // _fireballAppearPose.WhenUnselected += DestroyCurrentFireball;
-
-    _shootPose.WhenSelected += ShootFireball;
+    _fireballAppearPose.WhenSelected += PrepareFireball;
+    _fireballAppearPose.WhenUnselected += ShootFireball;
   }
 
   private void OnDisable()
   {
-    _fireballAppearPose.WhenSelected -= CreateFireball;
-    // _fireballAppearPose.WhenUnselected -= DestroyCurrentFireball;
-
-    _shootPose.WhenSelected -= ShootFireball;
+    _fireballAppearPose.WhenSelected -= PrepareFireball;
+    _fireballAppearPose.WhenUnselected -= ShootFireball;
   }
 
-  private void CreateFireball()
+  private void PrepareFireball()
   {
-    if (_currentFireball) return;
+    if (_fireballReadyEffect.isPlaying) return;
 
-    _currentFireball = Instantiate(
-      _fireballPrefab,
-      _fireballShootingPosition.position,
-      Quaternion.identity,
-      _fireballParent);
-  }
+    _fireballReadyEffect.Play();
 
-  private void DestroyCurrentFireball()
-  {
-    if (!_currentFireball) return;
-
-    Destroy(_currentFireball.gameObject);
-    _currentFireball = null;
   }
 
   private void ShootFireball()
   {
-    if (!_currentFireball) return;
+    if (!_fireballReadyEffect.isPlaying) return;
 
-    _currentFireball.transform.parent = null;
+    _fireballReadyEffect.Stop();
+
+    var _currentFireball = Instantiate(
+      _fireballPrefab,
+      _fireballShootingPosition.position,
+      Quaternion.identity);
+
     _currentFireball.Shoot(_fireballShootingDirection.forward);
-    _currentFireball = null;
   }
 }
